f9cc9db867d53447830c50437725eb9f5295d40e
dropout-tech <info@dropout.tw>
2026-08-09 23:11:17 +0800

    style: shift mobile explore numbers left

--- components/pages/explore-client.tsx
+++ components/pages/explore-client.tsx
@@ -373,7 +373,7 @@ function TodayStage({
                 </span>
               </span>
               <span
-                className="relative text-[26px] leading-none tracking-[0.1em] text-[#F2F2F2] tabular-nums"
+                className="relative mr-1.5 text-[26px] leading-none tracking-[0.1em] text-[#F2F2F2] tabular-nums"
                 style={{ fontFamily: "var(--font-barlow), sans-serif", fontWeight: 900 }}
               >
                 {s.num}
